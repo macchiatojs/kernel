e258f2a143fad3ddce61adbe861ff53b6f833d00
3imed-jaberi <imed_jebari@hotmail.fr>
2021-06-24 18:54:35 +0200

    add context factory/initializer class ⚡ ..

---
+++ src/context.ts
@@ -0,0 +1,48 @@
+import { IncomingMessage, ServerResponse } from 'http'
+import Kernel from './kernel'
+import Request from './request'
+import Response from './response'
+
+/**
+ * Initializer
+ *
+ * @class Context
+ * @param {Kernel} app
+ * @param {Request} request
+ * @param {Response} response
+ * @api public
+ */
+class Context {
+  app: Kernel
+  config: Map<string, unknown>
+  rawRequest: IncomingMessage
+  rawResponse: ServerResponse
+  request:Request
+  response: Response
+
+  constructor(
+    app: Kernel,
+    config: Map<string, unknown>,
+    request: IncomingMessage,
+    response: ServerResponse
+  ) {
+    // add the app instance.
+    this.app = app
+    // add the config Map.
+    this.config = config
+
+    // add raw request and raw response to context.
+    this.rawRequest = request
+    this.rawResponse = response
+
+    // make request and response.
+    this.request = new Request(request)
+    this.response = new Response(response, request)
+
+    // initialize the request and the response.
+    this.request.initialize(config, this.response)
+    this.response.initialize(config, this.request)
+  }
+}
+
+export default Context

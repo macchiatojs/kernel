7acce67829e2183ac525112d1ee0bb7eafbbcd1c
3imed-jaberi <imed_jebari@hotmail.fr>
2021-08-14 19:19:05 +0200

    add test for cookies integration 🧪 ..

---
+++ test/request/request.cookies.spec.ts
@@ -0,0 +1,22 @@
+import Cookies from 'cookies'
+import assert from 'assert'
+import request from 'supertest'
+import Kernel, { Request } from '../../src'
+
+describe('request', () => {
+  let app: Kernel
+
+  beforeEach(() => {
+    app = new Kernel()
+  })
+
+  describe('.cookies', () => {
+    it('should exist cookies inside the request', async () => {
+      app.use((request: Request) => {
+        assert(request.cookies instanceof Cookies)
+      });
+
+      await request(app.start()).get('/')
+    })
+  })
+})
---
+++ test/response/response.cookies.spec.ts
@@ -0,0 +1,22 @@
+import Cookies from 'cookies'
+import assert from 'assert'
+import request from 'supertest'
+import Kernel, { Request, Response } from '../../src'
+
+describe('response', () => {
+  let app: Kernel
+
+  beforeEach(() => {
+    app = new Kernel()
+  })
+
+  describe('.cookies', () => {
+    it('should exist cookies inside the request', async () => {
+      app.use((request: Request, response: Response) => {
+        assert(response.cookies instanceof Cookies)
+      });
+
+      await request(app.start()).get('/')
+    })
+  })
+})

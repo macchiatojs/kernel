4a26939e6d5faaea344d58ed743db49e3bdd1c7d
3imed-jaberi <imed_jebari@hotmail.fr>
2021-10-30 20:26:10 +0200

    imprv types 🍰 ..

--- src/types.ts
+++ src/types.ts
@@ -7,8 +7,8 @@ import type Response from './response'
  * @types
  */
 export type Next<T=unknown> = () => Promise<T>
-export type KoaStyleHandler<C=Context, N=Next<unknown>, R=unknown> = (context: C, next: N) => R
-export type ExpressStyleHandler<Req=Request, Res=Response, N=Next<unknown>, R=unknown> = (request: Req, response: Res, next: N) => R
+export type KoaStyleHandler<C=Context, N=Next, R=unknown> = (context: C, next: N) => R
+export type ExpressStyleHandler<Req=Request, Res=Response, N=Next, R=unknown> = (request: Req, response: Res, next: N) => R
 export type MacchiatoHandler = ExpressStyleHandler|KoaStyleHandler
 export type onErrorHandler<T = Error> = (err?: T) => void
 export type KeyValueObject<T = unknown> = { [key:string]: T }

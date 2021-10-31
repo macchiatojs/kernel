bfd9dfbed6dd2b75ef55f1afe5250db7f89d3862
3imed-jaberi <imed_jebari@hotmail.fr>
2021-10-31 18:53:17 +0100

    comment typings ?? ..

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,1 +1,9 @@
-declare module 'hashlru'
+// declare module 'hashlru'
+
+// export default function HLRU(max: number): {
+//   has: (key: string | number) => boolean
+//   remove: (key: string | number) => void
+//   get: (key: string | number) => any
+//   set: (key: string | number, value: any) => void
+//   clear: () => void
+// }
